5d7394c8f9aa319b981043bae53df431b503ade8
adityagoel28 <adityagoel2002@gmail.com>
2023-03-02 11:35:45 +0530

    NLP model added

--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from transformers import pipeline
 
+nlp = pipeline(task='sentiment-analysis',
+               model='nlptown/bert-base-multilingual-uncased-sentiment')
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -12,5 +15,26 @@ def feedbackk(request):
     field4 = request.POST['field4']
     field5 = request.POST['field5']
     print(field1, field2, field3, field4, field5)
-    context = {'hotels': 'aa'}
+
+    result = nlp(field1)
+
+    sent = ''
+    if (result[0]['label'] == '1 star'):
+        sent = 'very negative'
+    elif (result[0]['label'] == '2 star'):
+        sent = 'negative'
+    elif (result[0]['label'] == '3 stars'):
+        sent = 'neutral'
+    elif (result[0]['label'] == '4 stars'):
+        sent = 'positive'
+    else:
+        sent = 'very positive'
+
+    prob = result[0]['score']
+
+    # Format and return results
+    a =  {'sentiment': sent, 'probability': prob}
+
+    context = {'hotels': a}
+    print(a)
     return render(request, 'feedback.html', context)
